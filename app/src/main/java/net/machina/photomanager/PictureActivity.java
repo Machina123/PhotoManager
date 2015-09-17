51212c2882fa7390182b3e229f5cd964c8e43e12
Patryk Ciepiela <machina123@boun.cr>
2015-09-17 09:58:51 +0200

    WRITE_EXTERNAL_STORAGE permission added

--- app/src/main/java/net/machina/photomanager/PictureActivity.java
+++ app/src/main/java/net/machina/photomanager/PictureActivity.java
@@ -1,14 +1,37 @@
 package net.machina.photomanager;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import net.machina.photomanager.common.Constants;
+
+import java.io.File;
 
 public class PictureActivity extends AppCompatActivity {
 
     protected ListView listDirs;
+
+    protected File startingDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+    @Override
+    protected void onStart() {
+        File myDir = new File(startingDir.getPath() + "/" + Constants.APP_FOLDER + "/");
+        if(!myDir.exists()){
+            boolean success = myDir.mkdirs();
+            if(success) {
+                Toast.makeText(PictureActivity.this, "Stworzono katalog aplikacji", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(PictureActivity.this, "Nie stworzono katalogu aplikacji", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onStart();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/net/machina/photomanager/common/Constants.java
@@ -0,0 +1,6 @@
+package net.machina.photomanager.common;
+
+public class Constants {
+    public static final String APP_FOLDER = "MachiPhotos";
+    public static final String LOGGER_TAG = "MachiPhotos";
+}
